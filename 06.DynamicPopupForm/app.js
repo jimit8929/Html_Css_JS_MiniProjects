c13ea2b7457f1ae317277da6066e3c41ca32f7e1
jimit8929 <jimit8929@gmail.com>
2025-05-10 20:32:13 +0530

    PopUp form using tailwindCSS

--- 06.DynamicPopupForm/app.js
+++ 06.DynamicPopupForm/app.js
@@ -1,12 +1,26 @@
 const close = document.getElementById("close");
 const open = document.getElementById("open");
+const eye = document.querySelector("#eye-icon");
+const input = document.querySelector("#password");
 
 const model = document.getElementById("modal");
 
-open.addEventListener("click" , ()=>{
-    model.classList.remove("hidden");
+open.addEventListener("click", () => {
+  model.classList.remove("hidden");
 });
 
-close.addEventListener("click" , ()=>{
-    model.classList.add("hidden");
+close.addEventListener("click", () => {
+  model.classList.add("hidden");
+});
+
+eye.addEventListener("click", () => {
+  if (input.type === "password") {
+    input.type = "text";
+    eye.classList.remove("fa-eye");
+    eye.classList.add("fa-eye-slash");
+  } else {
+    input.type = "password";
+    eye.classList.remove("fa-eye-slash");
+    eye.classList.add("fa-eye");
+  }
 });
