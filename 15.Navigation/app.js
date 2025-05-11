de6f9c60e4f38741eb74a8df7fdbdb9daea0f10f
jimit8929 <jimit8929@gmail.com>
2025-05-12 03:13:07 +0530

    comments added for better understanding of logic

--- 15.Navigation/app.js
+++ 15.Navigation/app.js
@@ -4,22 +4,22 @@ const sections = document.querySelectorAll(".sections > section");
 
 menulist.forEach(li => {
   li.addEventListener("click", e => {
-    document.querySelectorAll(".menu_Links ul li.active")
+    document.querySelectorAll(".menu_Links ul li.active")  // previouslly if koi active ho toh usme se active remove kardo
       .forEach(activeEl => activeEl.classList.remove("active"));
 
-    const clickedLi = e.currentTarget;
-    clickedLi.classList.add("active");
+    const clickedLi = e.currentTarget; 
+    clickedLi.classList.add("active");  // jo click kiya usko hi active rakho
 
 
     sections.forEach(sec => {
       sec.classList.add("hidden");
-      sec.classList.remove("visible");
+      sec.classList.remove("visible");  // saare sections meh se visible hatado aur hidden add kardo jo taki jo click huwa hai wahi dikhega
     });
 
     const targetName = clickedLi.textContent.trim();
     sections.forEach(sec => {
       if (sec.dataset.section.trim() === targetName) {
-        sec.classList.remove("hidden");
+        sec.classList.remove("hidden");         // wahi show karo jiska section clicked-tab se match hojaye
         sec.classList.add("visible");
       }
     });
@@ -28,7 +28,7 @@ menulist.forEach(li => {
 
 document.addEventListener("DOMContentLoaded", () => {
   sections.forEach(sec => {
-    if (!sec.classList.contains("visible")) {
+    if (!sec.classList.contains("visible")) {  //inially refresh karne pe about section hi dikhega kyuki usme hi manually active add kiya hai
       sec.classList.add("hidden");
     }
   });
